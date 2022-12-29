6c0eaa98c1dc3d295b46aa452a591ec0f98dd97c
Navaneeth Krishna K <navaneeth.red@gmail.com>
2022-12-29 12:11:24 +0530

    Updated Favicon and Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,18 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Jiji Memes</title>
+        <meta name="HandheldFriendly" content="True" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel='canonical' href='https://pics.navaneeth.live/' />
+        <meta name="description" content="An advanced image sharing platform from Jiji." />
+      </Head>
       <Component {...pageProps} />
       <ToastContainer />
     </>
